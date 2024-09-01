4022befb142d9d70c3b39603288fae5359fdf252
Ivylucyawuor <awuorivy09>
2024-09-01 15:34:07 +0300

    Anagram

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, possible_anagrams):
+        sorted_word = ''.join(sorted(self.word))
+        matches = []
+        
+        for candidate in possible_anagrams:
+            if ''.join(sorted(candidate)) == sorted_word:
+                matches.append(candidate)
+        
+        return matches
